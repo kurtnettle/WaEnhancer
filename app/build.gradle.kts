bea57cd6a2f15093b1686cb4d889069d91b33a20
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2026-07-14 02:33:45 +0600

    Revert "chore(build): remove x86 and x86_64 ABI filters" (fixes `Dev4Mod/WaEnhancer Issue#726`)
    This reverts commit f24dd55e20863c0fdd236ba225b495c060ce5935.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,8 @@ android {
         ndk {
             abiFilters.add("armeabi-v7a")
             abiFilters.add("arm64-v8a")
+            abiFilters.add("x86_64")
+            abiFilters.add("x86")
         }
 
         buildConfigField("Boolean", "RESET_ON_INSTALL", "false")
